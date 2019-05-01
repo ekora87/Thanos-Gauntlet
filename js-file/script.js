388c6a22612824847bbf4224728d42531e535e6a
Beyonce Knowles <beyonce@thinkful.com>
2019-04-30 20:02:37 -0500

    Desktop Thanos Commit

--- js-file/script.js
+++ js-file/script.js
@@ -1,6 +1,24 @@
-// $(function() {
-//     $('header').on('click', '#guantlet', function(event) {
-//       alert("NOOOO");
-//     });
 
-alert("NOOOOO");
+$('#guantlet').click(function() {
+    //$('.article-section, .characters').fadeOut(3000);
+    //while ($('.article-section').length > 0) {
+    // let num = $('.article-section').length;
+    //     do {
+          
+    //      var random = Math.floor(Math.random()*10);
+    //         $(".characters-icon").eq(random).fadeOut(1000);
+    //     num = num -1;
+    //     console.log("length: " + num);
+    //     }
+    //     while (num >= 0);
+        
+    
+        setInterval(function(){
+            if ($('.article-section').length > 0) {
+                var random = Math.floor(Math.random()*3);
+                $(".article-section").eq(random).fadeOut(1000);
+            }
+          }, 2000);
+});
+
+
